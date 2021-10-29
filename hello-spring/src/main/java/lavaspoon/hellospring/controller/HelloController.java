8f331346d245ae7e67c7fffa8b4d46df8930dc4a
lavaspoon <lavaspoon@naver.com>
2021-10-29 18:43:04 +0900

    api 정상작동 테스트

---
+++ hello-spring/src/main/java/lavaspoon/hellospring/controller/HelloController.java
@@ -0,0 +1,45 @@
+package lavaspoon.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello") //localhost:8080/'hello' 로 던진걸 받음
+    public String hello(Model model) {
+        model.addAttribute("data","hello!!");//'hello!!' 데이터를 직접받음
+        return "hello";//hello.html 로 가서 렌더링
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
+        model.addAttribute("name",name); //key, value
+        return "hello-templete";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String hellostring(@RequestParam(value = "name", required = true) String name) {
+        return "hello" + name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
