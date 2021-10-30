a60e33d3517ad8a1f3327694477296415f174926
lavaspoon <lavaspoon@naver.com>
2021-10-30 22:18:11 +0900

    회원가입 테스트케이스 작성 및 테스트

--- hello-spring/src/main/java/lavaspoon/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/lavaspoon/hellospring/repository/MemoryMemberRepository.java
@@ -7,30 +7,34 @@ import java.util.*;
 //구현체
 public class MemoryMemberRepository implements MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private static Map<Long, Member> store = new HashMap<>(); //Key: Long, Value: Member
     private static long sequence = 0L;
 
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
-        store.put(member.getId(), member);
+        store.put(member.getId(), member); //(id, member)
         return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
+        return Optional.ofNullable(store.get(id)); //store.get(id)가 Null이여도 반환
     }
 
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
+                .filter(member -> member.getName().equals(name)) //파라미터로 받은 name 이랑 getName이랑 일치하는지 확인하여 필터링
+                .findAny();//찾으면 반환
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
